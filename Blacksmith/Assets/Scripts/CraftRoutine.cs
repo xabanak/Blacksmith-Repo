e6c670813e99090324de7eeabb237360200216f2
grimscyth <grimscyth@gmail.com>
2015-04-01 15:42:30 -0700

    Updated UI Scripting

--- Blacksmith/Assets/Scripts/CraftRoutine.cs
+++ Blacksmith/Assets/Scripts/CraftRoutine.cs
@@ -1,11 +1,33 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class CraftRoutine : MonoBehaviour {
+	public GameObject heatSliderObject;
+	public GameObject hammerSliderObject;
+	public GameObject timerSliderObject;
+	public GameObject furnaceSliderObject;
+	private Slider heatSlider;
+	private Slider hammerSlider;
+	private Slider timerSlider;
+	private Slider furnaceSlider;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start () 
+	{
+		GameObject heatBck = GameObject.Find ("heatSliderObject/Background");
+		Image heatBckImage = heatBck.GetComponent<Image> ();
+		heatBckImage.sprite = Resources.Load ("Images/heat scale1.png") as Sprite;
+
+		heatSlider = heatSliderObject.GetComponent<Slider> ();
+		hammerSlider = hammerSliderObject.GetComponent<Slider> ();
+		timerSlider = timerSliderObject.GetComponent<Slider> ();
+		furnaceSlider = furnaceSliderObject.GetComponent<Slider> ();
+
+		heatSlider.value = 0;
+		hammerSlider.value = 0;
+		timerSlider.value = 0;
+		furnaceSlider.value = 0;
 	}
 	
 	// Update is called once per frame
@@ -14,6 +36,6 @@ public class CraftRoutine : MonoBehaviour {
 	}
 
 	void fixedUpdate () {
-
+		heatSlider.value += 1;
 	}
 }
