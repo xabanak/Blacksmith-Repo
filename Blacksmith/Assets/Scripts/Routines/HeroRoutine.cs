f7b84e88b5a4ad00f0bc193787e3a3be7f7cfdb5
grimscyth <grimscyth@gmail.com>
2015-04-06 11:40:48 -0700

    Hero UI Additions
    Made it possible to have up to 3 active heroes at a time and enable them
    as you acquire more active heroes.

--- Blacksmith/Assets/Scripts/Routines/HeroRoutine.cs
+++ Blacksmith/Assets/Scripts/Routines/HeroRoutine.cs
@@ -4,23 +4,60 @@ using System.Collections;
 
 public class HeroRoutine : MonoBehaviour 
 {
+    public int hiredHeroes;
     private int minutes;
     private int seconds;
 
-    public float timer = 10; // timer to track hero return time
-    public Text heroReturnText;
+    public float timer1 = 10; // timer to track hero return time of first hero
+    public float timer2 = 20; // timer to track hero return time of first hero
+    public float timer3 = 30; // timer to track hero return time of first hero
+    public Text heroReturnText1;
+    public Text heroReturnText2;
+    public Text heroReturnText3;
+
+
 
 
 	// Use this for initialization
 	void Start () 
     {
-        heroReturnText.text = "Doom in: 100 seconds";
+        hiredHeroes = 0;
+        heroReturnText1.text = "Hero returns: 0:00";
+        heroReturnText2.text = "Hero returns: 0:00";
+        heroReturnText3.text = "Hero returns: 0:00";
+        heroReturnText1.enabled = false;
+        heroReturnText2.enabled = false;
+        heroReturnText3.enabled = false;
+
 	}
 	
 	// Update is called once per frame
 	void Update () 
     {
-        timer -= Time.deltaTime;
+        if (heroReturnText1.enabled == true)
+        {
+            timer1 -= Time.deltaTime;
+            UpdateHeroTimer(timer1, heroReturnText1);
+        }
+
+        if (heroReturnText2.enabled == true)
+        {
+            timer2 -= Time.deltaTime;
+            UpdateHeroTimer(timer2, heroReturnText2);
+        }
+
+        if (heroReturnText3.enabled == true)
+        {
+            timer3 -= Time.deltaTime;
+            UpdateHeroTimer(timer3, heroReturnText3);
+        }
+
+        // test method
+        SetHeroActiveText();
+	}
+
+    void UpdateHeroTimer(float timer, Text heroReturnText)
+    {
         minutes = (int)(timer / 60.0f);
         seconds = (int)timer % 60;
         if (seconds >= 10)
@@ -31,9 +68,28 @@ public class HeroRoutine : MonoBehaviour
         {
             heroReturnText.text = "Hero returns: " + minutes + ":0" + seconds;
         }
-        else
+        else if (seconds == 0 && minutes == 0)
         {
             heroReturnText.text = "Hero has returned!";
         }
-	}
+    }
+
+    void SetHeroActiveText ()
+    {
+        if (hiredHeroes == 1)
+        {
+            heroReturnText1.enabled = true;
+        }
+
+        else if (hiredHeroes == 2)
+        {
+            heroReturnText2.enabled = true;
+        }
+
+        else if (hiredHeroes == 3)
+        {
+            heroReturnText3.enabled = true;
+        }
+
+    }
 }
