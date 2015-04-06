22223ba6b562e5176203c31c9434da202bb36d38
xabanak <justinus@live.com>
2015-04-06 13:31:15 -0700

    Added the StageTimeMatrix logic
    Added all the logic into the StageTimeMatrix script to read in a text
    file for the values of the multipliers and added a public getter so
    other scripts can pass in an item name and a material type to get the
    time multiplier for crafting timer.

--- Blacksmith/Assets/Scripts/Matrix/StageTimeMatrix.cs
+++ Blacksmith/Assets/Scripts/Matrix/StageTimeMatrix.cs
@@ -1,15 +1,52 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.IO;
+using System;
 
-public class StageTimeMatrix : MonoBehaviour {
+public class StageTimeMatrix : MonoBehaviour 
+{
+    const int matrixLength = 1;
+    const int matrixWidth = 9;
 
-	// Use this for initialization
-	void Start () {
-	
+    double[,] stageTimeMatrix;
+    enum Item
+    {
+        Sword = 0,
+    }
+    enum Material
+    {
+        Tin = 0, 
+        Copper, 
+        Bronze,
+        Brass,
+        Iron,
+        BlackenedIron,
+        Steel,
+        SteelAlloy,
+        Titanium
+    }
+	void Start () 
+    {
+	    stageTimeMatrix = new double[matrixLength, matrixWidth];
+        readTextFile("Assets/Resources/stageTime.txt");
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    void readTextFile(string filePath)
+    {
+        StreamReader inputStream = new StreamReader(filePath);
+
+        for (int i = 0; i < matrixLength; i++)
+        {
+            for (int j = 0; j < matrixWidth; j++)
+            {
+                stageTimeMatrix[0, j] = Convert.ToDouble(inputStream.ReadLine());
+            }
+        }
+    }
+    public double getMultiplier(string item, string material)
+    {
+        int arrayLength = (int)Enum.Parse(typeof(Item), item);
+        int arrayWidth = (int)Enum.Parse(typeof(Material), material);
+
+        return stageTimeMatrix[arrayLength, arrayWidth];
+    }
 }
