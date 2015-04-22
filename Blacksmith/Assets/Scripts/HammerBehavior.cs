8cdab015707eb0b14afe9fd739e139cd1eae91f3
xabanak <justinus@live.com>
2015-04-22 15:29:44 -0700

    Improved hammer logic
    Vastly improved hammer logic and feel of hammering

--- Blacksmith/Assets/Scripts/HammerBehavior.cs
+++ Blacksmith/Assets/Scripts/HammerBehavior.cs
@@ -5,20 +5,22 @@ public class HammerBehavior : MonoBehaviour {
 
 
     private Vector3 screenPoint;
-    private Vector3 offset;
+    public Vector3 offset;
     private Vector3 resetPoint;
+    private const float angleChange = 35.0f;
 
     public GameObject anvil;
     public CraftRoutine craftController;
 
     public bool snapBack;
-
+    private bool hasHit;
     private bool isDragged;
     // Use this for initialization
     void Start()
     {
         resetPoint = gameObject.transform.position;
         isDragged = false;
+        hasHit = false;
     }
 
     void Update()
@@ -28,19 +30,28 @@ public class HammerBehavior : MonoBehaviour {
             Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
 
             Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint);
-            transform.position = curPosition;
+            transform.position = curPosition + offset;
         }
+        if (hasHit)
+        {
+            transform.rotation = Quaternion.Euler(transform.rotation.x, transform.rotation.y, transform.rotation.z - angleChange);
+            isDragged = false;
+            transform.position = resetPoint;
+        }
+
     }
 
     void OnTriggerEnter2D(Collider2D myCollider)
     {
         if (myCollider.gameObject.name == "Anvil")
         {
+            hasHit = true;
             craftController.hammerHitOnAnvil();
         }
+        
     }
 
-    void OnTriggerExit2D(Collider2D myCollider)
+    /*void OnTriggerExit2D(Collider2D myCollider)
     {
         if(myCollider.gameObject.name == "Component")
         {
@@ -49,20 +60,30 @@ public class HammerBehavior : MonoBehaviour {
                 isDragged = false;
                 transform.position = resetPoint;
             }
+            hasHit = false;
         }
-    }
+    }*/
     void OnMouseDown()
     {
         screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
-        offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+        //offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+        transform.rotation = Quaternion.Euler(transform.rotation.x, transform.rotation.y, transform.rotation.z + angleChange);
 
         isDragged = true;
     }
 
     void OnMouseUp()
     {
-        isDragged = false;
-        transform.position = resetPoint;
+        if (hasHit)
+        {
+            hasHit = false; ;
+        }
+        else
+        {
+            transform.rotation = Quaternion.Euler(transform.rotation.x, transform.rotation.y, transform.rotation.z - angleChange);
+            isDragged = false;
+            transform.position = resetPoint;
+        }
     }
 
     /*void OnMouseDrag()
