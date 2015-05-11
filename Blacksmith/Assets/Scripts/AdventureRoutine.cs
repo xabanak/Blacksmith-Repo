c61220544ebb4e01a5adb010204225f83483f726
xabanak <justinus@live.com>
2015-05-11 15:22:01 -0700

    Added two sounds for the grinder stage

---
+++ Blacksmith/Assets/Scripts/AdventureRoutine.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AdventureRoutine : MonoBehaviour 
+{
+    private Adventurer[] adventurers;
+    const int NUM_ADVENTURERS = 3;
+    private int numAdventurers;
+
+	void Start () 
+    {
+	    adventurers = new Adventurer[NUM_ADVENTURERS];
+        numAdventurers = 0;
+	}
+
+	void Update () 
+    {
+	
+	}
+
+    public void addAdventurer(Adventurer newHero)
+    {
+        if (numAdventurers == 3)
+        {
+            return; 
+        }
+
+        adventurers[numAdventurers] = newHero;
+
+        numAdventurers++;
+    }
+}
+
+class Adventurer
+{
+    private string name;
+    private int level;
+    private int powerLevel;
+
+    Adventurer(string name, int level)
+    {
+        this.name = name;
+        this.level = level;
+        powerLevel = 0;
+    }
+}
